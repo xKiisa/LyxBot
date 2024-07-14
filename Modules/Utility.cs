33f4cba4c79a39d640c3c5c3a013bca5194cd610
Kisa <104528108+xKiisa@users.noreply.github.com>
2024-07-14 22:48:50 +0200

    Create Utility.cs
    Added command to retrieve a user's avatar

---
+++ Modules/Utility.cs
@@ -0,0 +1,23 @@
+﻿using DSharpPlus.Commands;
+using DSharpPlus.Entities;
+
+namespace LyxBot.Modules
+{
+    public class Utility
+    {
+        public class Avatar
+        {
+            [Command("avatar")]
+            public static async Task AvatarAsync(CommandContext ctx, DiscordUser? member = null)
+            {
+                member ??= ctx.User;
+                var avatar = member.AvatarUrl;
+                DiscordEmbedBuilder embed = new();
+                {
+                    embed.ImageUrl = avatar;
+                    await ctx.RespondAsync(embed: embed);
+                }
+            }
+        }
+    }
+}
