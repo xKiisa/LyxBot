d92a56fb98b2fe083f262d3208b24348ba03e882
Kisa <104528108+xKiisa@users.noreply.github.com>
2024-07-17 23:11:28 +0200

    Kick command

---
+++ Modules/Moderation.cs
@@ -0,0 +1,24 @@
+﻿using DSharpPlus.Commands;
+using DSharpPlus.Entities;
+using DSharpPlus.Commands.ContextChecks;
+
+namespace LyxBot.Modules
+{
+    public class Moderation
+    {
+        public class Kick
+        {
+            [Command("kick")]
+            [RequirePermissions(DiscordPermissions.KickMembers)]
+            public static async Task KickAsync(CommandContext ctx, DiscordMember? member = null)
+            {
+                if (member is null)
+                {
+                    return;
+                }
+                await member.RemoveAsync();
+                await ctx.RespondAsync($"Kicked {member.Mention} from the server!");
+            }
+        }
+    }
+}
